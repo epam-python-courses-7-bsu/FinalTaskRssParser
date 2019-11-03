36f2db15923382c80cd1ca1eedc5f8a78223910c
Vlad Protosevich <protosevich2001@gmail.com>
2019-11-03 16:00:56 +0300

    Iteration 3

---
+++ final_task/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name="rss-reader",
+    version='3.5',
+    packages=find_packages(),
+    author="Vlad Protosevich",
+    author_email="protosevic2001@gmail.com",
+    install_requires=['feedparser==5.2.1'],
+    entry_points={
+        'console_scripts': [
+            'rss-reader = rss_parser.rss_parser:run'
+        ]
+    },
+)
