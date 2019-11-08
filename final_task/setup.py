dbecad71a1631c7cf57f56e457e3fc749d6c49ce
IlyaTorch <itorch2001@gmail.com>
2019-11-08 16:12:30 +0300

    Iteration 1 Add content of setup.py and add fiel __init__ in rss-reader directory

---
+++ final_task/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rss-reader',
+    version='1.0',
+    packages=find_packages(),
+    description='Python RSS reader',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/IlyaTorch/FinalTaskRssParser',
+    author='Ilya Torch',
+    author_email='itorch2001@gmail.com',
+    install_requires=['feedparser==5.2.1'],
+    entry_points={
+        'console_scripts': [
+            'rss-reader = rss_reader.rss_reader:main',
+        ],
+    },
+)
