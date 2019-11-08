266d8685efb724d691538bb78a9762d92da460c9
IlyaTorch <itorch2001@gmail.com>
2019-11-08 21:01:43 +0300

    Iteration 2 Solve problem with wrapping package. Add opportunity wrapping into distribution package

--- final_task/rss_reader/rss_reader.py
+++ final_task/rss_reader/rss_reader.py
@@ -1,11 +1,20 @@
+import os
+import sys
+import logging
+this_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(this_dir)
 from Handler import Handler
 from Logging import logging_decorator
 from ConsoleParse import get_arguments_from_console
-import logging
 
 
 @logging_decorator
 def main():
+    arg_parser_args = get_arguments_from_console()
+    version = 1.0
+
+    if arg_parser_args.verbose:
+        logging.basicConfig(level=logging.INFO)
     if arg_parser_args.limit <= 0:
         arg_parser_args.limit = 1
     handler = Handler(arg_parser_args.source, arg_parser_args.limit, version)
@@ -21,9 +30,5 @@ def main():
 
 
 if __name__ == "__main__":
-    arg_parser_args = get_arguments_from_console()
-    version = 1.0
 
-    if arg_parser_args.verbose:
-        logging.basicConfig(level=logging.INFO)
     main()
