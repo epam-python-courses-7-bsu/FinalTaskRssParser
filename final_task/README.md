933ba355b49c5c3915685b99e3f415f416b4e9fc
Leonid-Shutov <14m64y87w@gmail.com>
2019-11-09 13:07:36 +0300

    Iteration 1 bugs fixed. Feed, date and alt added. Requirements file fixed. The description of JSON added to README.md

--- final_task/README.md
+++ final_task/README.md
@@ -1,3 +1,5 @@
-# Your readme here
-Some text.
-Checkout how to write this file using *markdown*.
+This rss-reader uses requests library to get requests from news websites.
+BeautifulSoup from bs4 library enables to find all "items" (pieces of news) from rss feed.
+The components of news are devided into groups (title, description, image, link) using dictionary.
+News are converted into JSON format using json library method "dump".  
+Example of JSON format: {"title": "Germany Has Been Unified for 30 Years. Its Identity Still Is Not.", "date": "Fri, 08 Nov 2019 11:35:06 +0000", "description": "East Germans, bio-Germans, passport Germans: In an increasingly diverse country, the legacy of a divided history has left many feeling like strangers in their own land.", "link": "https://www.nytimes.com/2019/11/08/world/europe/germany-identity.html?emc=rss&partner=rss", "image": "https://static01.nyt.com/images/2019/11/05/world/xxgermany-identity-promo/xxgermany-identity27-moth-v2.jpg"}
--- final_task/rss_reader/rss_reader.py
+++ final_task/rss_reader/rss_reader.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 import requests
 import argparse
 import json
@@ -13,25 +14,27 @@ log = logging.getLogger("Log")
 def find_news(url):
     """Parses news"""
     logging.info("Searching for news on " + url)
-    while True:
-        try:
-            resp = requests.get(url)
-            soup = BeautifulSoup(resp.content, features="xml")
-            items = soup.findAll('item')
-            log.info(str(len(items))+" pieces of news have been found")
-        except requests.exceptions.MissingSchema:
-            log.error("Wrong URL")
-            print("Wrong URL: " + url)
-            print("Try to input a correct URL: ")
-            url = input()
-        except requests.exceptions.ConnectionError:
-            log.error("Can't connect to the " + url)
-            print("Can't connect to the " + url)
-            print("Try to input a different URL: ")
-            url = input()
-        else:
-            break
+    try:
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.content, features="xml")
+        items = soup.findAll('item')
+        log.info(str(len(items)) + " pieces of news have been found")
+    except requests.exceptions.MissingSchema:
+        log.error("Wrong URL")
+        print("Wrong URL: " + url)
+        sys.exit()
+    except requests.exceptions.ConnectionError:
+        log.error("Can't connect to the " + url)
+        print("Can't connect to the " + url)
+        sys.exit()
     return items
+
+
+def find_channel(url):
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, features="xml")
+    channel = soup.find('channel')
+    return channel
 
 
 def collect_news(items, limit) -> list:
@@ -41,74 +44,82 @@ def collect_news(items, limit) -> list:
         try:
             news_item = {}
             news_item['title'] = item.title.text
-            news_item['description'] = item.description.text
+            news_item['date'] = item.pubDate.text
+            soup = BeautifulSoup(item.description.text,'lxml')
+            news_item['description'] = soup.text
             news_item['link'] = item.link.text
-            if item.thumbnail != None:
+            if item.thumbnail:
                 news_item['image'] = item.thumbnail['url']
-            elif item.content != None:
+            elif item.content:
                 news_item['image'] = item.content['url']
             else:
                 news_item['image'] = item.enclosure['url']
+            if soup.find('img'):
+                news_item['alt'] = soup.find('img')['alt']
+            elif item.find('media:text'):
+                soup = BeautifulSoup(item.find('media:text').text, 'lxml')
+                news_item['alt'] = soup.find('img')['alt']
+            else:
+                news_item['alt'] = None
         except TypeError:
             log.error("Could not load an image")
             news_item['image'] = "No image"
+        except AttributeError:
+            log.error("Probably no description")
+            news_item['description'] = "No description"
         news.append(news_item)
         if len(news) == limit:
             break
     return news
 
 
-def print_news(list_of_news):
+def print_news(list_of_news, channel):
+    print("Feed: " + channel.title.text + '\n\n')
     for item in list_of_news:
         print("Title: " + item['title'])
+        print("Date: " + item['date'])
         print("Description: " + item['description'])
         print("Image: " + item['image'])
+        if item['alt']:
+            print("Alt: " + item['alt'])
         print("Link: " + item['link'])
         print('------------------')
 
 
 def json_convert(news):
     """Converts the news into JSON format"""
-    with open("README.md", "w") as write_file:
+    with open("JSON.json", "w") as write_file:
         json.dump(news, write_file)
 
 
 if __name__ == '__main__':
     
-    while True:
-        try:
-            parser = argparse.ArgumentParser(description="RSS")
-            parser.add_argument('source', type=str, help='RSS URL')
-            parser.add_argument('--version', action='store_true', help='Print version info')
-            parser.add_argument('--json', action='store_true', help='Print result as JSON in stdout')
-            parser.add_argument('--verbose', action='store_true', help='Outputs verbose status messages')
-            parser.add_argument('--limit', type=int, default=999, help='Limit news topics if this parameter provided')
-            args = parser.parse_args()
-        except SyntaxError:
-            print("Wrong syntax in args")
-        else:
-            break
-
-if args.version:
-    print("Version is 1.0")
+    try:
+        parser = argparse.ArgumentParser(description="RSS")
+        parser.add_argument('source', type=str, help='RSS URL')
+        parser.add_argument('--version', action='store_true', help='Print version info')
+        parser.add_argument('--json', action='store_true', help='Print result as JSON in stdout')
+        parser.add_argument('--verbose', action='store_true', help='Outputs verbose status messages')
+        parser.add_argument('--limit', type=int, default=999, help='Limit news topics if this parameter provided')
+        args = parser.parse_args()
+    except SyntaxError:
+        print("Wrong syntax in args")
+        sys.exit()
+    
+    if args.version:
+        print("Version is 1.1")
     if args.verbose:
         logging.basicConfig(level=logging.INFO)
-
-if args.limit:
-    if args.limit > 0:
-        limit = args.limit
+    
+    if args.limit:
+        if args.limit > 0:
+            limit = args.limit
         else:
             limit = 5
-
-items = find_news(args.source)
-news = collect_news(items, limit)
-print_news(news)
+    channel = find_channel(args.source)
+    items = find_news(args.source)
+    news = collect_news(items, limit)
+    print_news(news, channel)
 
 if args.json:
     json_convert(news)
-
-
-
-
-
-
