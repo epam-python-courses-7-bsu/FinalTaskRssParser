4d9e10a64d287d7c67ec1c166053b7aae73f31c2
Leonid-Shutov <14m64y87w@gmail.com>
2019-11-14 23:26:21 +0300

    Iteration 2

---
+++ final_task/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+      name='rss_reader',
+      version='2.0',
+      packages=find_packages(),
+      description='RSS-reader',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/Leonid-Shutov/FinalTaskRssParser',
+      author='Leonid Shutov',
+      author_email='14m64y87w@gmail.com',
+      install_requires=['beautifulsoup4==4.8.1','lxml==4.4.1','requests==2.22.0','soupsieve==1.9.4'],
+      entry_points={
+      'console_scripts': [
+                          'rss-reader = rss_reader.rss_reader:main',
+                          ],
+      },
+      )
+
