941b8537a699fcedca9663c9b6373fa86ade544b
Stacy <st.merkush@gmail.com>
2019-11-19 02:21:08 +0300

    Fix bags

--- final_task/rss_reader/rss_get_items.py
+++ final_task/rss_reader/rss_get_items.py
@@ -3,50 +3,48 @@ import logging as log
 import urllib.error
 import urllib.request
 from bs4 import BeautifulSoup
+from collections import defaultdict
 
 
-def get_items(url):
+def get_items(url: str):
     request = urllib.request.Request(url)
-    item = {}
     try:
-        response = urllib.request.urlopen(request)
-        soup = BeautifulSoup(response, "html.parser")
+        soup_xml = BeautifulSoup(
+            urllib.request.urlopen(request),
+            "xml"
+        )
     except (urllib.error.URLError, KeyError):
         log.info('URLError, pls try other one')
     else:
         log.info('Connect to website is fine')
+        items = []
+        xml_items = list(soup_xml.find_all('item'))
+        for rss_node_xml in xml_items:
+            item_xml = defaultdict(list)
+            for node in rss_node_xml.findChildren():
+                key = node.name
+                value = node.text
+                if key == "content":
+                    item_xml[key].append(node.attrs['url'])
+                else:
+                    item_xml[key] = value
+            items.append(item_xml)
 
-    for item_node in soup.find_all('item'):
-        for setitem_node in item_node.findChildren():
-            key = setitem_node.name
-            value = setitem_node.text
-            item[key] = value
-    return item
-
-
-""" Try to delete all html in description """
-
-
-def description(item):
-    return BeautifulSoup(item, "html.parser").getText()
-
-
-def pubdate(pub_date):
-    pub_date = (pub_date.split(' ')[1:5])
-    new_pub_date = datetime.strptime(pub_date, '%Y-%m-%d').date()
-    return new_pub_date
+        return items
 
 
-"""  Check if there is media content """
+def description(description_element: str) -> str:
+    """ Try to delete all html in description """
+    return BeautifulSoup(description_element, "html.parser").getText()
 
 
-def media_content(item):
-    media = ''
-    if 'media_content' in item.keys():
-        media = item.media_content[0]['url']
-    elif 'media_thumbnail' in item.keys():
-        media = item.media_thumbnail[0]['url']
-    return media
+def pubdate(pub_date: str) -> str:
+    """reformat publication date"""
+    if pubdate:
+        # pub_date = datetime.datetime(*map(int, pub_date.split(' ')[1:5]))
+        # new_pub_date = datetime.datetime.strftime(pub_date, '%Y-%m-%d')
+        return pub_date
+    return pub_date
 
 
 def get_json(item):
