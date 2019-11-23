b7df68649bec212a553e9dc6da16592179eb0eed
Stacy <st.merkush@gmail.com>
2019-11-23 12:53:24 +0300

    fix

--- final_task/rss_reader/check.py
+++ final_task/rss_reader/check.py
@@ -4,7 +4,7 @@ import urllib.request
 
 def internet_on():
     try:
-        urllib.error.urlopen('http://216.58.192.142', timeout=5)
+        urllib.error.urlopen("http://216.58.192.142", timeout=10)
         return True
-    except urllib.request.URLError as err:
+    except urllib.request.URLError:
         return False
