2d33cc37f7df20eafd808a53a1fb4127337e85c6
Stacy <st.merkush@gmail.com>
2019-11-23 11:54:23 +0300

    Fix

--- final_task/rss_reader/rss_reader.py
+++ final_task/rss_reader/rss_reader.py
@@ -7,7 +7,8 @@ import arguments
 import printers
 import rss_get_items
 import converting
-
+import DatabaseEmulation
+from copy import deepcopy
 
 now = datetime.datetime.now()
 
@@ -25,56 +26,42 @@ def print_verbose() -> None:
         print(f.read())
 
 
-def get_txt_date(data) -> None:
-    date_id = {}
-    with open('dates.txt', 'r', encoding="utf-8") as f:
-        for line in f:
-            key, *value = line.split()
-            date_id[key] = value
-    # len_txt_date = len(date_id)
-    if data == date_id[key]:
-        date_id[key] = value
-        with open('{}.txt'.format(value), 'r', encoding='utf-8') as f:
-            print(f.read())
-    else:
-        print("There isn't any news from this day")
-
-
-def cash_news() -> None:
-    log.info('Try to cash news from stdout')
-    data_id = {}
-    data = now.strftime("%d/%m/%Y")
-    with open('dates.txt', 'r', encoding="utf-8") as f:
-        for line in f:
-            key, *value = line.split()
-            data_id[key] = value
-    len_txt_date = len(data_id) + 1
-    remembered_data = {}
-    with open('dates.txt', 'w', encoding='utf-8') as f:
-        remembered_data[data] = len_txt_date
-        f.write(str(remembered_data))
-    # with open('{}.txt'.format(remembered_data[data]),
-    # 'w', encoding='utf-8') as f:
-
-
 if __name__ == '__main__':
+
     log.info('Start script')
+
+    database = DatabaseEmulation.DatabaseEmulation(
+        '_database/dates.txt', '_database/data'
+    )
+
     received_args = arguments.command_line()
     link = received_args.URL
     limit = received_args.limit
     date = received_args.date
-    items = rss_get_items.get_items(link)
+
+    all_items = None
+
+    if date is not None:
+        if database.check_date(date):
+            idx = database.get_id(date)
+            all_items = database.get_items(idx)
+        else:
+            print('Nothing by this date')
+    else:
+        all_items = rss_get_items.get_items(link)
+
+    items = deepcopy(all_items)
 
     if limit is not None:
         log.info('User choose some limits')
-        items = items[:limit]
+        items = all_items[:limit]
 
     if received_args.json:
         log.info('User choose json format')
         printers.print_json(items)
     else:
         printers.print_news(items)
-        #cash_news()
+        database.write_items(all_items)
 
     if received_args.verbose:
         log.info('User choose verbose')
@@ -87,6 +74,3 @@ if __name__ == '__main__':
     if received_args.pdf:
         log.info('User choose pdf')
         converting.create_pdf(items)
-
-    if date is not None:
-        get_txt_date(date)
