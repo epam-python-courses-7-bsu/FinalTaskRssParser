d5497abeb8b7606c44133b1b8badc8180e1a886a
AnatoliAgeichik <43906902+AnatoliAgeichik@users.noreply.github.com>
2019-11-24 00:32:35 +0300

    Iteration 4

--- final_task/rss_reader/WorkWithCache.py
+++ final_task/rss_reader/WorkWithCache.py
@@ -1,22 +1,15 @@
 import json
-<<<<<<< HEAD
 import os
-=======
->>>>>>> 89c4f8b1295f477501cad9f19c1d2b63db6309a7
 
 from RssException import RssException
 # from Handler import News
 from Log import log_decore
-<<<<<<< HEAD
 import urllib.request
-=======
->>>>>>> 89c4f8b1295f477501cad9f19c1d2b63db6309a7
 
 '''
 pass the dictionary to a function and write to a file json
 '''
 
-<<<<<<< HEAD
 @log_decore
 def save_img(url, name):
     if os.path.exists("images"):
@@ -28,8 +21,6 @@ def save_img(url, name):
         os.makedirs("images")
         urllib.request.urlretrieve(url, f"images/{name}.jpg")
 
-=======
->>>>>>> 89c4f8b1295f477501cad9f19c1d2b63db6309a7
 
 @log_decore
 def write_json_to_cache(entry_dict):
@@ -42,33 +33,23 @@ def write_json_to_cache(entry_dict):
 
     if not [item_news for item_news in load_news if item_news["title"] == entry_dict["title"]]:
         load_news.append(entry_dict)
-<<<<<<< HEAD
         save_img(entry_dict["links"][0], entry_dict["title"][:15])
-=======
->>>>>>> 89c4f8b1295f477501cad9f19c1d2b63db6309a7
 
     with open("cache.json", "w") as cache:
         json.dump(load_news, cache, indent=3)
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 89c4f8b1295f477501cad9f19c1d2b63db6309a7
 '''
 read from the file and, if the date converges, return a list of these news
 '''
 
 
-<<<<<<< HEAD
 def read_from_file(date, lim):
     # here we look, if = -1, then we make a flag to read all the news
     flag = 0
     if lim == -1:
         flag = 1
-=======
-def read_from_file(date):
->>>>>>> 89c4f8b1295f477501cad9f19c1d2b63db6309a7
     try:
         entries = json.load(open("cache.json"))
     except json.JSONDecodeError:
@@ -78,12 +59,7 @@ def read_from_file(date):
     daily_news = []
     date = str(date)
     for item_entries in entries:
-<<<<<<< HEAD
         if item_entries["strDate"] == date and (lim or flag):
             daily_news.append(item_entries)
             lim -= 1
-=======
-        if item_entries["strDate"] == date:
-            daily_news.append(item_entries)
->>>>>>> 89c4f8b1295f477501cad9f19c1d2b63db6309a7
     return daily_news
