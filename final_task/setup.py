cb62489ce21b8526fd6ab31522deaf6dabf662dd
scarzdz <scar.zdz@gmail.com>
2019-11-24 22:50:31 +0300

    Add dependencies list and reformat file

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,29 +1,34 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='rss-reader_scarzdz',
-    version='0.1',
-    url='https://github.com/scarzdz/FinalTaskRssParser',
-    license='MIT',
-    author='Denis Marfonov',
-    author_email='marfonovdenis@gmail.com',
-    description='Cli-based RSS Reader',
+    name="rss-reader_scarzdz",
+    version="0.1",
+    url="https://github.com/scarzdz/FinalTaskRssParser",
+    license="MIT",
+    author="Denis Marfonov",
+    author_email="marfonovdenis@gmail.com",
+    description="Cli-based RSS Reader",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['rss_reader'],
+    packages=["rss_reader"],
     classifiers=[
         "Programming Language :: Python :: 3"
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
+    install_requires=[
+        "beautifulsoup4",
+        "feedparser",
+        "lxml"
+    ],
     entry_points={
-        'console_scripts': [
-            'rss-reader=rss_reader.rss_reader:main',
+        "console_scripts": [
+            "rss-reader=rss_reader.rss_reader:main",
         ],
     },
 )
