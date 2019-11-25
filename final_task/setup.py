51a1cfa832496904bcb24db770760b64d875cd69
AnatoliAgeichik <43906902+AnatoliAgeichik@users.noreply.github.com>
2019-11-25 23:40:37 +0300

    fixed bugs

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="Rss-reader",
-    version="1.0",
-    author="Anatoli Ageichik",
-    author_email="3anatoliageichik@gmail.com",
-    description="Rss reader, which write in Python",
-    url="https://github.com/AnatoliAgeichik/FinalTaskRssParser",
-    packages=['rss_reader'],
-    python_requires='>=3.8.0',
-    install_requires=['feedparser']
+    name='rss-reader',
+    version='4.0',
+    packages=find_packages(),
+    description='RSS reader',
+    url='https://github.com/AnatoliAgeichik/FinalTaskRssParser',
+    author='AnatoliAgeichik',
+    author_email='3anatoliageichik@gmail.com',
+    python_requires='>=3.7.0',
+    install_requires=['feedparser==5.2.1', 'fpdf==1.7.2', 'dominate==2.4.0'],
+    entry_points={
+        'console_scripts': [
+            'rss-reader = rss_reader.rss_reader:main',
+        ],
+    },
+    test_suite='rss_reader.tests'
 )
